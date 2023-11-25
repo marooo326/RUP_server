7317a0db8a00e8bbc32827f9be5e0939340c8045
marooo326 <ktnqkr01@gmail.com>
2023-11-26 01:14:19 +0900

    FIX: MemberService 수정

--- src/main/java/com/rup/apiPayload/code/ErrorStatus.java
+++ src/main/java/com/rup/apiPayload/code/ErrorStatus.java
@@ -10,12 +10,13 @@ import static org.springframework.http.HttpStatus.*;
 @AllArgsConstructor
 public enum ErrorStatus implements BaseCode {
     _INTERNAL_SERVER_ERROR(INTERNAL_SERVER_ERROR, 5000, "서버 에러, 관리자에게 문의 바랍니다."),
-    _BAD_REQUEST(BAD_REQUEST,4000,"잘못된 요청입니다."),
-    _UNAUTHORIZED(UNAUTHORIZED,4001,"로그인이 필요합니다."),
+    _BAD_REQUEST(BAD_REQUEST, 4000, "잘못된 요청입니다."),
+    _UNAUTHORIZED(UNAUTHORIZED, 4001, "로그인이 필요합니다."),
     _FORBIDDEN(FORBIDDEN, 4003, "금지된 요청입니다."),
 
+    _MEMBER_NOT_FOUND_EXCEPTION(NOT_FOUND, 4004, "존재하지 않는 회원입니다."),
 
-    INVALID_TOKEN_EXCEPTION(UNAUTHORIZED,4008,"토큰이 올바르지 않습니다." );
+    INVALID_TOKEN_EXCEPTION(UNAUTHORIZED, 4008, "토큰이 올바르지 않습니다.");
     private final HttpStatus httpStatus;
     private final Integer code;
     private final String message;
--- src/main/java/com/rup/domain/enums/DiscountPercentage.java
+++ src/main/java/com/rup/domain/enums/DiscountPercentage.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 public enum DiscountPercentage {
     THIRTY(30), FIFTY(50), SEVENTY(70);
 
-    private int percentage;
+    private final int percentage;
 
     DiscountPercentage(int percentage) {
         this.percentage = percentage;
