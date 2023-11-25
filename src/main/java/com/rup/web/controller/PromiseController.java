416242ea160d0025850f25fa0b2984f62e4bf02e
marooo326 <ktnqkr01@gmail.com>
2023-11-26 04:06:15 +0900

    docs: Swagger 명세 추가

--- src/main/java/com/rup/web/controller/PromiseController.java
+++ src/main/java/com/rup/web/controller/PromiseController.java
@@ -4,6 +4,9 @@ import com.rup.annotation.AuthMember;
 import com.rup.apiPayload.response.ResponseDto;
 import com.rup.domain.Promise;
 import com.rup.service.PromiseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,30 +25,50 @@ import static com.rup.web.dto.response.PromiseResponseDto.PromiseSummaryResponse
 public class PromiseController {
     private final PromiseService promiseService;
 
+    @Operation(summary = "약속 생성", description = "약속 생성 API 입니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "2000", description = "OK 성공"),
+    })
     @PostMapping
     public ResponseDto<PromiseSummaryResponseDto> createPromise(@AuthMember Long memberId, @RequestBody PromiseCreateDto createDto) {
         Promise promise = promiseService.createPromise(memberId, createDto);
         return ResponseDto.of(PromiseSummaryResponseDto.of(promise));
     }
 
+    @Operation(summary = "약속 참여 API", description = "약속참여 API 입니다. Body에 inviteCode를 넣어주시면 됩니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "2000", description = "OK 성공"),
+    })
     @PostMapping("/participate")
     public ResponseDto<PromiseSummaryResponseDto> participateInPromise(@AuthMember Long memberId, @RequestBody String inviteCode) {
         Promise promise = promiseService.participateInPromise(memberId, inviteCode);
         return ResponseDto.of(PromiseSummaryResponseDto.of(promise));
     }
 
+    @Operation(summary = "약속 전체 조회 API", description = "약속 전체조회 API입니다. 자신이 참여한 약속 전체를 조회합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "2000", description = "OK 성공"),
+    })
     @GetMapping("/")
     public ResponseDto<List<PromiseSummaryResponseDto>> getPromises(@AuthMember Long memberId) {
         List<Promise> promise = promiseService.getAllPromises(memberId);
         return ResponseDto.of(promise.stream().map(PromiseSummaryResponseDto::of).toList());
     }
 
+    @Operation(summary = "약속 단건 조회 API", description = "약속 단건조회 API입니다. 자신이 참여한 약속 중 하나를 조회합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "2000", description = "OK 성공"),
+    })
     @GetMapping("/{promiseId}")
     public ResponseDto<PromiseDetailResponseDto> getPromise(@AuthMember Long memberId, @PathVariable Long promiseId) {
         Promise promise = promiseService.getPromise(memberId, promiseId);
         return ResponseDto.of(PromiseDetailResponseDto.of(promise));
     }
 
+    @Operation(summary = "위치 업데이트 API", description = "위치를 업데이트하는 API입니다. Body에 위도, 경도, 주소를 넣어주시면 본인 위치를 업데이트하고, 나머지 멤버들의 위치를 반환합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "2000", description = "OK 성공"),
+    })
     @PostMapping("/{promiseId}/locations")
     public ResponseDto<List<LocationResponseDto>> updateLocation(
             @AuthMember Long memberId,
@@ -56,7 +79,10 @@ public class PromiseController {
         return ResponseDto.of(allLocations);
     }
 
-    //약속 완료 시키고 지각자들 전부 반환
+    @Operation(summary = "약속 완료 API", description = "약속을 완료시키는 API입니다. 약속을 완료시키고, 지각자들의 정보를 반환합니다. 해당 정보로 투표 진행하시면 됩니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "2000", description = "OK 성공"),
+    })
     @PostMapping("/{promiseId}/complete")
     public ResponseDto<List<MemberDetailResponseDto>> completePromise(@PathVariable Long promiseId) {
         return ResponseDto.of(promiseService.completePromise(promiseId));
