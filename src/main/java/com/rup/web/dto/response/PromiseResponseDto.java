2a477cc9ef1667d0313605ebfbebaa2339f28192
marooo326 <ktnqkr01@gmail.com>
2023-11-26 08:11:54 +0900

    FEAT: detail dto 수정

--- src/main/java/com/rup/web/dto/response/PromiseResponseDto.java
+++ src/main/java/com/rup/web/dto/response/PromiseResponseDto.java
@@ -4,6 +4,7 @@ import com.rup.domain.Promise;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class PromiseResponseDto {
 
@@ -42,17 +43,32 @@ public class PromiseResponseDto {
         private Long id;
         private String name;
         private LocalDateTime promiseTime;
+        private String promiseDate;
+        private String promiseHour;
+        private Long leftDate;
+        private Long leftHour;
+        private Long leftMinute;
         private Long penalty;
+        private String address;
         private String inviteCode;
         private String authorName;
         private String createdAt;
 
         public static PromiseDetailResponseDto of(Promise promise) {
+            Long day = ChronoUnit.DAYS.between(LocalDateTime.now(), promise.getPromiseTime());
+            Long hour = ChronoUnit.HOURS.between(LocalDateTime.now(), promise.getPromiseTime()) - day * 24;
+            Long minute = ChronoUnit.MINUTES.between(LocalDateTime.now(), promise.getPromiseTime()) - day * 24 * 60 - hour * 60;
             return PromiseDetailResponseDto.builder()
                     .id(promise.getId())
                     .name(promise.getName())
                     .promiseTime(promise.getPromiseTime())
+                    .promiseDate(promise.getPromiseTime().toLocalDate().toString())
+                    .promiseHour(promise.getPromiseTime().toLocalTime().toString().substring(0, 2))
+                    .leftDate(day)
+                    .leftHour(hour)
+                    .leftMinute(minute)
                     .penalty(promise.getPenalty())
+                    .address(promise.getLocation().getAddress())
                     .inviteCode(promise.getInviteCode())
                     .authorName(promise.getAuthor().getName())
                     .createdAt(promise.getCreatedAt().toString())
