95ad803060fa7d6fd96df88b4b05e1970c4cc034
HyoBN <qls0786@naver.com>
2023-11-25 17:01:29 +0900

    Swagger 세팅 추가

--- src/main/java/com/rup/web/controller/RootRestController.java
+++ src/main/java/com/rup/web/controller/RootRestController.java
@@ -1,10 +1,20 @@
 package com.rup.web.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RootRestController {
+
+    @Operation(summary = "테스트 API", description = "테스트 API 입니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "2000", description = "OK 성공"),
+    })
     @GetMapping("/health")
     String healthCheck(){
         return "RUP Server is healthy!!";
