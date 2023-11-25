a14996e43c138f4fc3d071dd42a033a552bd2a9f
HyoBN <qls0786@naver.com>
2023-11-25 16:01:04 +0900

    init commit

---
+++ src/main/java/com/rup/RupApplication.java
@@ -0,0 +1,13 @@
+package com.rup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RupApplication.class, args);
+	}
+
+}
